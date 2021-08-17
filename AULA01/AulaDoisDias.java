1afd093d75456052894322d23135a7d86505cdde
Andrey Gheno Piekas <andrey.piekas@gmail.com>
2021-08-17 19:58:11 -0300

    Update AulaDoisDias.java

--- AULA01/AulaDoisDias.java
+++ AULA01/AulaDoisDias.java
@@ -3,35 +3,31 @@ package fundamentos;
 import java.util.Scanner;
 
 public class AulaDoisDias {
-	
+
 	public static void main(String[] args) {
 		int primeiroDia = 0;
 		int segundoDia = 0;
-		
-		
+
 		Scanner entrada = new Scanner(System.in);
-		
+
 		System.out.println("Digite o primeiro dia: ");
 		primeiroDia = entrada.nextInt();
-		
+
 		System.out.println("Digite o segundo dia: ");
 		segundoDia = entrada.nextInt();
-		
-		if (primeiroDia > 1 && primeiroDia < 7 && segundoDia > 1 && segundoDia < 7 ) {
-			if((primeiroDia == 3 ^ segundoDia == 5) || (primeiroDia == 5 ^ segundoDia == 3)) {
+
+		if (primeiroDia > 1 && primeiroDia < 7 && segundoDia > 1 && segundoDia < 7) {
+			if ((primeiroDia == 3 ^ segundoDia == 5) || (primeiroDia == 5 ^ segundoDia == 3)) {
 				System.out.println("Matricula efetivada com sucesso!");
-			}else {
+			} else {
 				System.out.println("Não pode marcar aula terça e quinta!");
 			}
-			
-		}else {
+
+		} else {
 			System.out.println("Dia da semana inválido");
 		}
-		
+
 		entrada.close();
 	}
-	
-	
-	
 
 }
