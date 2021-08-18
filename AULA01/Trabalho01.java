8adac399c668952ceefd544f91ebc2c350e0b00c
Andrey Gheno Piekas <andrey.piekas@gmail.com>
2021-08-17 21:59:40 -0300

    Update Trabalho01.java

--- AULA01/Trabalho01.java
+++ AULA01/Trabalho01.java
@@ -5,9 +5,7 @@ import java.util.Scanner;
 public class Trabalho01 {
 	public static void main(String[] args) {
 		int total = 0;
-		double nota = 0.0;
-		double calculo = 0.0;
-		double nFinal = 0.0;
+		double nota = 0.0, calculo = 0.0, nFinal = 0.0;
 		Scanner entrada = new Scanner(System.in);
 
 		System.out.println("Informe a quantia de notas a ser informada: ");
@@ -17,10 +15,8 @@ public class Trabalho01 {
 			System.out.printf("Informe a nota %d: ", i);
 			nota = entrada.nextDouble();
 			calculo += nota;
-
 		}
 		nFinal = calculo / total;
-		System.out.println(calculo);
 		System.out.println("A nota final é de: " + nFinal);
 		entrada.close();
 	}
