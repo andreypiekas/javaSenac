8229640f66e39239b23abcbbfbd946c4d05737dd
Andrey Gheno Piekas <andrey.piekas@gmail.com>
2021-08-31 20:46:44 -0300

    classes

---
+++ classes/CoisasRedondas.java
@@ -0,0 +1,27 @@
+package classes;
+
+public class CoisasRedondas {
+	//atributos
+	static double pi = 3.14;
+	double raio;
+	
+	//construtores
+	public CoisasRedondas(double raioInserido) {
+		raio = raioInserido;
+	}
+	
+	//métodos
+	
+	double perimetroCircuferencia() {
+		return 2 * pi * raio;
+	}
+	
+	double areaCirculo() {
+		return pi * Math.pow(raio, 2);
+	}
+	
+	double volumeEsfera() {
+		return 4/3 * pi * (raio * raio * raio);
+	}
+
+}
---
+++ classes/CoisasRedondasMain.java
@@ -0,0 +1,29 @@
+package classes;
+
+public class CoisasRedondasMain {
+	public static void main(String[] args) {
+		CoisasRedondas rabico = new CoisasRedondas(2);
+		CoisasRedondas pizza = new CoisasRedondas(20);
+		CoisasRedondas bola = new CoisasRedondas(10);
+		
+		System.out.println("O rabico tem o comprimento: " + rabico.perimetroCircuferencia());
+		System.out.printf("O rabido tem o comprimento: %.2f \n", rabico.perimetroCircuferencia());
+		
+		System.out.println("A pizza tem área: " + pizza.areaCirculo());
+		System.out.printf("A pizza tem áreaA pizza tem a área: %.2f \n", pizza.areaCirculo());
+		
+		System.out.println("A pizza tem área: " + bola.volumeEsfera());
+		System.out.printf("A pizza tem áreaA pizza tem a área: %.2f \n", bola.volumeEsfera());
+		
+		CoisasRedondas.pi = 3.1;
+		if (rabico.pi == pizza.pi) {
+			System.out.println("Os valores de pi são iguais.");
+		}else
+			System.out.println("Os valores de pi não não iguais.");
+		
+		
+		
+	
+	}
+
+}
