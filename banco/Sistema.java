5f86996b19918a891336512eb0cf61a12d201c80
Andrey Gheno Piekas <andrey.piekas@gmail.com>
2021-09-21 21:51:34 -0300

    Add files via upload

---
+++ banco/Sistema.java
@@ -0,0 +1,42 @@
+package banco;
+
+import banco.dados.*;
+import banco.funcionarios.*;
+
+
+
+public class Sistema {
+	public static void main(String[] args) {
+		Cliente cliente1 = new Cliente("José", "Silva", 12345678999L);
+		Cliente cliente2 = new Cliente("José", "Silva", 12345678999L);
+		Cliente cliente3 = new Cliente("José", "Silva", 12345678999L);
+		Conta conta1 = new Conta(cliente1, 1, 1, 0, 1000);
+		Conta conta2 = new Conta(cliente2, 0, 0, 0, 0);
+		Conta conta3 = new Conta(cliente3, 0, 0, 0, 0);
+		
+		
+		//Como ver quantas contas foram criadas
+		//Static fez com que o gerente  --------- terminar o comentario
+		System.out.println("Hoje temos cadastrado "+ Conta.getNumeroContas() + " contas em nosso sistema");
+		
+		//Metodos de interface
+		conta1.depositar(200);
+		conta1.sacar(100);
+		
+		System.out.println("R$ "+conta1.getSaldo());
+		System.out.println(cliente1.getNomeESobrenome());
+		
+		Funcionario funcionario1 = new Funcionario("Maria", "Rocha", 12345678998L, 1000);
+		System.out.printf("Salario de %s é de R$ %.2f \n", funcionario1.getNomeESobrenome(), funcionario1.getSalario());
+		//Um gerente é um funcionario, por isso acessa os metodos de funcionario
+		Gerente gerente1 = new Gerente("Fabio", "Silva", 12345678997L, 10000, 15, "fabio123");
+		System.out.printf("Salario de %s é de R$ %.2f \n", gerente1.getNomeESobrenome(), gerente1.getSalario());
+		
+		System.out.println(gerente1.valida("senha"));
+		System.out.println(gerente1.valida("fabio123"));
+		
+		
+	
+	}
+
+}
---
+++ banco/dados/Conta.java
@@ -0,0 +1,74 @@
+package banco.dados;
+
+public class Conta {
+	//Atributos
+	//Conta tem um objeto da classe cliente
+	//Isso é composição
+	private Cliente titular;
+	private int conta;
+	private int agencia;
+	private double saldo;
+	private double limite;
+	//um atributo para armazenar quantas contas o banco tem
+	//cada vez que uma conta é criada eu aumento esse atributo
+	private static int numeroContas;
+	
+	//Construtor
+	public Conta(Cliente titular, int conta, int agencia, double saldo, double limite){
+		this.titular = titular;
+		this.conta = conta;
+		this.agencia = agencia;
+		this.saldo = saldo;
+		this.limite = limite;
+		numeroContas++;
+	}
+	
+	//Metodos
+	void depositar(double valor) {
+		if(valor > 0) {
+			saldo += valor;
+			System.out.println("Deposito - Operação realizada com sucesso!");
+		}
+		else {
+			System.out.println("Deposito - Valor não permitido para deposito!");
+		}
+
+	}
+	
+	void sacar(double valor) {
+		if(valor > 0) {
+			if (valor <= saldo + limite) {
+				saldo -= valor;
+				System.out.println("Saque - Operação realizada com sucesso!");
+			}
+			else {
+				System.out.println("Saque - Saldo insuficiente para saque!");
+			}
+		}
+		else {
+			System.out.println("Saque - Valor nao permitido para saque!");			
+		}
+		
+	}
+
+	//Metodos getters
+	public double getSaldo() {
+		//Lógica do negócio do banco
+		//Enganar que tu tem saldo quando não tem
+		return saldo+limite;
+		
+	}
+	
+	public static int getNumeroContas() {
+		return numeroContas;
+	}
+	
+	//Metodos setters
+	//Nao faz sentido para meu negocio do sistemade um banco
+	//As outras partes do programa nao devem setar o saldo
+	/*public void setSaldo(double valor) {
+		saldo = valor;
+		
+	}*/
+	
+}
